698f470daa92cf472dab2cddd4520ff3303598e6
Waddles <79166144+WaddlesDiscord@users.noreply.github.com>
2021-11-02 11:49:56 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,17 @@
-var app = new Vue({
-	el:'#app',
-	data: {
-		message: "test"
-	  }
-});
+var app;
+
+function load(){
+	loadVue();	
+};
+
+function loadVue(){
+	app = new Vue({
+		el:"#app",
+		data:{
+			player,
+			VERSION,
+			mouseX,
+			mouseY,
+		}
+	})
+}
