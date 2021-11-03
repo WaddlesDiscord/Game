eeab0d52403c7877b55f8349864672284c1b3d24
Waddles <79166144+WaddlesDiscord@users.noreply.github.com>
2021-11-03 13:37:43 +0000

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,1 +1,18 @@
-
+function loadVue(){
+  Vue.component('upgrade',{
+    template: '<button>a</button>'
+  })
+  
+  
+  
+	app = new Vue({
+		el:"#app",
+		data:{
+			player,
+			VERSION,
+			mouseX,
+			mouseY,
+			upgrade1: true
+		}
+	})
+}
